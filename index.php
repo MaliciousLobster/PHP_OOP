8771a68cb4df26416f0056eda9fdfff4f6787fef
NateM <Mr.Awsmness@gmail.com>
2015-01-06 20:42:17 -0800

    Test push to see if repository was set up correctly

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+	class Classroom{
+		public $teacher;
+		public $room;
+		public $subject;
+
+		function WhatClassroom(){
+			echo "{$this->teacher}" .
+			"{$this->room}" .
+			"{$this->subject}";
+		}
+	}
+
+	$APP = new Classroom();
+	$APP->teacher = "Dan";
+	$APP->room = "E206";
+	$APP->subject = "Computer Science";
+
+	asdsfd
